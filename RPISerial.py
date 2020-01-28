425ccc361a707fa566d122596a26a7ae56780bdc
YongTat <tanyongtat.tyt@gmail.com>
2020-01-28 16:56:24 +0800

    added to do

--- RPISerial.py
+++ RPISerial.py
@@ -8,29 +8,34 @@ def initserial():
     ser.baudrate = 9600
     ser.timeout = 2
     ports = lp.comports(include_links=False)
+    #Finding the port the arduino is connected to
     for port in ports:
-        print(port.device)
-        print(port.manufacturer)
-	manufacturer = port.manufacturer
-	if "arduino" in manufacturer.lower():
-		ser.port = port.device
-		break
+        #print(port.device)
+        #print(port.manufacturer)
+        manufacturer = port.manufacturer
+        if "arduino" in manufacturer.lower():
+            ser.port = port.device
+            break
     ser.open()
 
 def readfromserial():
     line = ser.readlines()
-    print(line)
+    return line
 
 def writetoserial(stringwrite):
     newstringwrite = stringwrite + "\n"
     ser.write(newstringwrite.encode())
 
+"""
+To do next, use list.sort to sort the list
+"""
+
 def main():
     initserial()
-    readfromserial()
+    print(readfromserial())
     scannerin = input("Scan: ")
     writetoserial(str(scannerin))
-    readfromserial()
+    print(readfromserial())
 
 if __name__ == "__main__":
     main()
