006100fd24d28a19b1e882741e5a57038c59d119
YongTat <tanyongtat.tyt@gmail.com>
2020-01-31 14:20:09 +0800

    Test case for lighting up 30 LED

--- RpiLora.py
+++ RpiLora.py
@@ -1,5 +1,16 @@
 from SX127x.LoRa import *
 from SX127x.board_config import BOARD
+import time
+
+"""
+A function to simulate picking list from server
+"""
+def inittester(scannerin):
+    zerotoend = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+    if scannerin == "1":
+        return zerotoend
+    else:
+        return zerotoend.reverse()
 
 def lorainit():
     global lora
@@ -24,7 +35,14 @@ def sender(asciilist):
     lora.set_mode(MODE.RXCONT)
 
 def main():
-    pass
+    lorainit()
+    scannerin = input("Scanner Input")
+    pickinglist = inittester(scannerin)
+    pickinglistascii = stringtoascii(pickinglist)
+    for item in pickinglistascii:
+        sender(item)
+        time.sleep(1000)
+
 
 if __name__ == "__main__":
     main()
