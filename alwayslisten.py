a58881aa7524db64971c617fb3712ab6a1bf76b9
YongTat <tanyongtat.tyt@gmail.com>
2020-02-19 10:53:00 +0800

    Less output spam

--- alwayslisten.py
+++ alwayslisten.py
@@ -10,18 +10,12 @@ def lorainit():
     lora.set_mode(MODE.STDBY)
     lora.set_freq(915.0) #set to 915MHz for SG use
 
-"""
-Always listens and prints out packets
-"""
-def alwayslisten():
-    lora.set_mode(MODE.RXCONT) #Switch to recieve mode
-    payload = lora.read_payload(nocheck=True)
-    print(bytes(payload).decode("utf-8",'ignore'))
-
 def main():
     lorainit()
+    lora.set_mode(MODE.RXCONT) #Switch to recieve modes
     while True:
-        alwayslisten()
+        if (payload = lora.read_payload(nocheck=True)):
+            print(bytes(payload).decode("utf-8",'ignore'))
         time.sleep(0.1)
 
 if __name__ == "__main__":
