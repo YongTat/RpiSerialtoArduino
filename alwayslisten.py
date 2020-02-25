c9c83cad2487bcdf085f9e30f5759b53bc5bd67f
YongTat <tanyongtat.tyt@gmail.com>
2020-02-25 16:36:58 +0800

    Thingsspeak POST for sensor data

--- alwayslisten.py
+++ alwayslisten.py
@@ -3,6 +3,11 @@ from SX127x.board_config import BOARD
 import time
 import requests
 
+thingsspeakapikeys = {
+    "A5": "5J9C9RM334D695X3",
+    "A4": "1J14O7KG9I2JB7GY"
+}
+
 def lorainit():
     global lora
 
@@ -22,12 +27,18 @@ def main():
 
             if (text.find("S") > 0):
                 pos = text.find("S")
+                # payload = {
+                #     "Name": name,
+                #     "Temp": text[pos+1:pos+4],
+                #     "Humidity": text[pos+4:pos+7]
+                # }
+                # r = requests.post("http://192.168.137.142:1880/Sensorin", data=payload)
                 payload = {
-                    "Name": name,
-                    "Temp": text[pos+1:pos+4],
-                    "Humidity": text[pos+4:pos+7]
+                    "api_key": thingsspeakapikeys.get(name),
+                    "field1": text[pos+1:pos+3],
+                    "field2": text[pos+4:pos+6],
                 }
-                r = requests.post("http://192.168.137.142:1880/Sensorin", data=payload)
+                r = requests.post("https://api.thingspeak.com/update.json", data=payload)
 
             elif (text.find("L") > 0):
                 pos = text.find("L")
@@ -35,7 +46,7 @@ def main():
                             "Name": name,
                             "Data": text[pos+1:pos+2]
                         }
-                r = requests.post("http://192.168.137.142:1880/LEDin", data=payload)
+                r = requests.post("http://localhost:1880/LEDin", data=payload)
             
             payload = []
             lora.set_mode(MODE.SLEEP)
