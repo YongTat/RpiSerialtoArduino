b87a19ba7cc91053bb78b17d64fedd657d48c57e
YongTat <tanyongtat.tyt@gmail.com>
2020-03-05 13:44:02 +0800

    multiple field for things speak Test

--- asynctest.py
+++ asynctest.py
@@ -6,7 +6,17 @@ import requests
 
 thingsspeakapikeys = {
     "A5": "5J9C9RM334D695X3",
-    "A4": "1J14O7KG9I2JB7GY"
+    "A4": "1J14O7KG9I2JB7GY",
+    "0" : "K0GBV7LQY0FWSWNB",
+    "1" : "5WYCGS9GF263QFO4"
+}
+
+fieldnumber = {
+    "1" : ["field1", "field2"],
+    "2" : ["field3", "field4"],
+    "3" : ["field5", "field6"],
+    "4" : ["field7", "field8"]
+
 }
 
 def lorainit():
@@ -63,6 +73,9 @@ def listenmode():
 
             if (text.find("S") > 0):
                 pos = text.find("S")
+                id = (int(text[1:pos])-1) / 4
+                print(id)
+                field = (int(text[1:pos])-1) % 4
                 # payload = {
                 #     "Name": name,
                 #     "Temp": text[pos+1:pos+4],
@@ -70,9 +83,9 @@ def listenmode():
                 # }
                 # r = requests.post("http://192.168.137.142:1880/Sensorin", data=payload)
                 payload = {
-                    "api_key": thingsspeakapikeys.get(name),
-                    "field1": text[pos+1:pos+3],
-                    "field2": text[pos+4:pos+6],
+                    "api_key": thingsspeakapikeys.get(id),
+                    field[0]: text[pos+1:pos+3],
+                    field[1]: text[pos+4:pos+6],
                 }
                 r = requests.post("https://api.thingspeak.com/update.json", data=payload)
 
